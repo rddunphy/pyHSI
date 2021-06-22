3b7ad0556d83a523a296832e95268db2c6b7f5bf
R. David Dunphy <rdd.dunphy@gmail.com>
2021-06-22 17:01:37 +0100

    Icon adjustments

--- icons.sh
+++ icons.sh
@@ -9,18 +9,18 @@ size_hidpi=40
 
 for f in ${icons[@]}; do
 	echo -e "\nConverting $f..."
-	inkscape -o icons/$f$size.png -h $size -w $size icons/$f.svg
-	inkscape -o icons/$f$size_hidpi.png -h $size_hidpi -w $size_hidpi icons/$f.svg
+	inkscape --export-png icons/$f$size.png -h $size -w $size icons/$f.svg
+	inkscape --export-png icons/$f$size_hidpi.png -h $size_hidpi -w $size_hidpi icons/$f.svg
 done
 
 echo -e "\nConverting application icon to png..."
-inkscape -o icons/pyhsi.png -h 256 -w 256 icons/pyhsi.svg
+inkscape --export-png icons/pyhsi.png -h 256 -w 256 icons/pyhsi.svg
 
 echo -e "\nConverting application icon to ico..."
 mkdir tmp
 declare -a sizes=(16 32 48 128 256)
 for s in ${sizes[@]}; do
-	inkscape -o tmp/$s.png -h $s -w $s icons/pyhsi.svg
+	inkscape --export-png tmp/$s.png -h $s -w $s icons/pyhsi.svg
 done
 convert tmp/16.png tmp/32.png tmp/48.png tmp/128.png tmp/256.png icons/pyhsi.ico
 rm -r tmp
